b6419d4c0d095c5de0c792b259ba920caeee4454
Bruno Grande <bgrande@sfu.ca>
2015-09-17 21:54:37 -0700

    remove console.log

--- js/agency.js
+++ js/agency.js
@@ -8,7 +8,6 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
-        console.log($anchor.height())
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
